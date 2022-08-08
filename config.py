06d00f5b32bcbffc46e3c70f5f04995349a0e49e
Arina_Nichko <oryna_nichko@epam.com>
2022-08-08 16:28:25 +0300

    New commit

--- config.py
+++ config.py
@@ -7,15 +7,16 @@ from aiogram import Bot, types
 
 TOKEN = os.getenv('BOT_TOKEN')
 print(TOKEN)
-service_bot = Bot(token='SERVICE_BOT_TOKEN')
+service_bot = Bot(token='BOT_TOKEN')
 print(service_bot)
-client_bot = Bot(token='BOT_TOKEN')
+client_bot = Bot(token='BOT_TOKEN_CLIENT')
 dp = Dispatcher(service_bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=5000)
 
 
